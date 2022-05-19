8cc9b5da18d7ca92a75e576af3cac7bee03e83b7
asimokby <asemokbey@gmail.com>
2022-05-19 21:25:57 +0300

    Made WriteToFile method save the dictionary in a separate file for

--- src/main/java/ContextFreeGrammar/ContextFreeGrammar.java
+++ src/main/java/ContextFreeGrammar/ContextFreeGrammar.java
@@ -114,17 +114,20 @@ public class ContextFreeGrammar {
     public void writeToFile(String fileName){
         try {
             FileWriter fw = new FileWriter(fileName);
+            FileWriter fww = new FileWriter(fileName+"_dictionary");
             for (Rule rule:rules){
                 fw.write(rule.toString() + "\n");
             }
             String dictionaryKeyString = "", dictionaryValueString = "";
             for (Map.Entry<String, Integer> set : dictionary.entrySet()) {
+                fww.write(set.getKey() + " -> " + set.getValue() + "\n");
                 dictionaryKeyString = dictionaryKeyString + " " + set.getKey();
                 dictionaryValueString = dictionaryValueString + " " + set.getValue();
             }
             fw.write("dictionary_keys:" + dictionaryKeyString.substring(1) + "\n");
             fw.write("dictionary_values:" + dictionaryValueString.substring(1) + "\n");
             fw.close();
+            fww.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
